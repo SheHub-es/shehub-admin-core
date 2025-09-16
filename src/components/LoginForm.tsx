e2feacaa69f2a5c552196706851430c4c10beec6
Irina Ichim <onlyirina7@gmail.com>
2025-09-16 13:24:07 +0200

    update Post auth

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -37,7 +37,7 @@ export default function LoginForm({ onRestart }: LoginFormProps) {
     setIsLoading(true);
 
     try {
-     const res = await fetch("/auth/login", {
+     const res = await fetch("/auth/admin/login", {
   method: "POST",
   headers: { "Content-Type": "application/json" },
   body: JSON.stringify({
